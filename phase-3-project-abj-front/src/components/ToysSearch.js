e617156ffe7ae21cb0cea1b25c3a2034a216db89
WizardofHause <jonrhause@gmail.com>
2022-11-17 13:03:44 -0700

    created search functionality on toys category page

--- phase-3-project-abj-front/src/components/ToysSearch.js
+++ phase-3-project-abj-front/src/components/ToysSearch.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function SearchTwo(){
-    return(
-        <div>
-
+function ToysSearch({ onSearch, search }) {
+    return (
+        <div className="suspect-container">
+            <h1>Toys!</h1>
+            <input
+                id="search"
+                type="text"
+                placeholder="Search..."
+                value={search}
+                onChange={onSearch}
+            />
         </div>
-        
     )
 }
 
-export default SearchTwo
+export default ToysSearch
