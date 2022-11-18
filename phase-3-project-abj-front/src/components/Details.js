e4e887852d895318ea49e8e045e45402fa6656db
WizardofHause <jonrhause@gmail.com>
2022-11-17 17:32:41 -0700

    made delete

--- phase-3-project-abj-front/src/components/Details.js
+++ phase-3-project-abj-front/src/components/Details.js
@@ -3,10 +3,15 @@ import { useParams } from "react-router-dom"
 import InputForm from "./InputForm";
 
 function Details() {
-
+    const [review, setReview] = useState([])
     const [item, setItem] = useState(null)
 
     const { id } = useParams()
+
+  function handleDeleteReview(id) {
+    const updatedReview = review.filter((r) => r.id !== id);
+    setReview(updatedReview);
+  }
 
     useEffect(() => {
         fetch(`http://localhost:9292/items/${id}`)
@@ -18,17 +23,17 @@ function Details() {
 
     if (!item) return <div>!</div>;
 
-    const { name, price, description, image, brand_name, reviews } = item
+    const { name, price, description, image, brand_name, category, reviews } = item
 
     return (
         <>
-        <br></br>
             <div className="cardInfo">
                 <img className="imageSize" src={image} alt="loading..." />
                 <p>Name: {name}</p>
-                <p>Brand: {brand_name}</p>
                 <p>Price: ${price}.00</p>
                 <p>Description: {description}</p>
+                <p>Brand: {brand_name}</p>
+                <p>Category: {category}</p>
             </div>
             <br></br>
             <InputForm />
@@ -36,12 +41,16 @@ function Details() {
 
             <div className="reviewInfo">
                 {reviews.map((review) => {
+                    function handleDelete() {
+                        fetch(`http://localhost:9292/reviews/${review.id}`, { method: "DELETE", })
+                        handleDeleteReview(review.id)
+                    }
                     return (
                         <div className="reviews">
                             <p>User Id: {review.user_id}</p>
                             <p>Stars: {review.star_rating}</p>
                             <p>Comments: {review.comment}</p>
-                            <button>Delete:</button>
+                            <button onClick={handleDelete}>Delete:</button>
                         </div>
                     )
                 })}
@@ -51,5 +60,3 @@ function Details() {
 }
 
 export default Details
-
-
